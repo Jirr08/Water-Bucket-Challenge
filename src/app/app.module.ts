aac2516bb3e1bbfa2fcc1f37c41684c81ca9e50c
Jose <jirr081696@gmail.com>
2023-02-06 02:05:46 -0400

    ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BucketsComponent } from './components/buckets/buckets.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BucketsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule
